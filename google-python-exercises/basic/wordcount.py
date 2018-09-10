809458d607487734f0945d99473a6696faaf1a74
tramontini <matheustramon@gmail.com>
2018-09-09 23:36:07 -0300

    Finished wordcount exercise

--- google-python-exercises/basic/wordcount.py
+++ google-python-exercises/basic/wordcount.py
@@ -45,7 +45,42 @@ import sys
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
 
-###
+def word_count_dict(filename):
+  """Returns a word/count dict for this filename."""
+  # Utility used by count() and Topcount().
+  word_count = {}  # Map each word to its count
+  input_file = open(filename, 'r')
+  for line in input_file:
+    words = line.split()
+    for word in words:
+      word = word.lower()
+      if not word in word_count:
+        word_count[word] = 1
+      else:
+        word_count[word] = word_count[word] + 1
+  input_file.close()  
+  return word_count
+
+
+def print_words(filename):
+  word_count = word_count_dict(filename)
+  words = sorted(word_count.keys())
+  for word in words:
+    print word, word_count[word]
+
+
+def get_count(word_count_tuple):
+  return word_count_tuple[1]
+
+
+def print_top(filename):
+  word_count = word_count_dict(filename)
+
+  items = sorted(word_count.items(), key=get_count, reverse=True)
+
+  # Print the first 20
+  for item in items[:20]:
+    print item[0], item[1]
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
